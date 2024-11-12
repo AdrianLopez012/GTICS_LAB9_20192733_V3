66eb7c7a6681ce9556ebb1e7412420cf7b06a919
AdrianLopez012 <142950326+AdrianLopez012@users.noreply.github.com>
2024-11-11 21:20:16 -0500

    Add files via upload

---
+++ lab20192733a/src/main/java/com/example/lab20192733a/Lab20192733aApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab20192733a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab20192733aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab20192733aApplication.class, args);
+	}
+
+}
---
+++ lab20192733a/src/test/java/com/example/lab20192733a/Lab20192733aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab20192733a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab20192733aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
